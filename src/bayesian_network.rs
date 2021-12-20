d504fd3bf0f1f356d7a606fd2302c408d0415080
rodrigobraz <rodrigobraz@fb.com>
2021-12-20 14:46:10 -0800

    Changing test to be tolerant of numerical noise

--- src/bayesian_network.rs
+++ src/bayesian_network.rs
@@ -4,6 +4,7 @@ use rsdd::manager::rsbdd_manager::{BddManager, BddWmc};
 use rsdd::repr::bdd::BddPtr;
 use rsdd::repr::cnf::Cnf;
 use rsdd::repr::var_label::{Literal, VarLabel};
+use assert_approx_eq::assert_approx_eq;
 
 /// A probability
 /// Can be symbolic (to be substituted in later for a known probability)
@@ -95,7 +96,7 @@ impl BayesianNetwork {
     pub fn new(shape: Vec<usize>, cpts: Vec<CPT>) -> BayesianNetwork {
         // sort the CPTs in topological order
         let mut sorted : Vec<CPT> = Vec::new();
-        let mut added : HashSet<usize> = HashSet::new(); 
+        let mut added : HashSet<usize> = HashSet::new();
         fn add(sorted: &mut Vec<CPT>, added: &mut HashSet<BnVar>, cpts: &Vec<CPT>, cpt: &CPT) -> () {
             if added.contains(&cpt.var) { return () };
             // add the parents
@@ -254,7 +255,7 @@ impl CompiledBayesianNetwork {
         let mut weights = self.weights.clone();
         // insert all the weights from the symbol table
         for (bnlbl, highw) in st.symbol_map.iter() {
-            // translate bnlbl into 
+            // translate bnlbl into
             let bddlbl = self.symbolic_map[bnlbl];
             weights.insert(bddlbl, (1.0 - highw, *highw));
         }
@@ -285,7 +286,7 @@ fn test_marginal_0() {
         CPT::new(0, vec![], HashMap::from([
             (vec![0], Probability::Concrete(0.1)),
             (vec![1], Probability::Concrete(0.9))
-            ])), 
+            ])),
     ];
     let bn = BayesianNetwork::new(shape, cpts);
     let mut compiled = CompiledBayesianNetwork::new(&bn, CompileMode::BottomUpChaviraDarwicheBDD);
@@ -302,13 +303,13 @@ fn test_marginal_1() {
         CPT::new(0, vec![], HashMap::from([
             (vec![0], Probability::Concrete(0.1)),
             (vec![1], Probability::Concrete(0.9))
-            ])), 
+            ])),
          CPT::new(1, vec![0], HashMap::from([
             (vec![0, 0], Probability::Concrete(0.3)),
             (vec![0, 1], Probability::Concrete(0.7)),
             (vec![1, 0], Probability::Concrete(0.4)),
             (vec![1, 1], Probability::Concrete(0.6)),
-            ])), 
+            ])),
     ];
     let bn = BayesianNetwork::new(shape, cpts);
     let mut compiled = CompiledBayesianNetwork::new(&bn, CompileMode::BottomUpChaviraDarwicheBDD);
@@ -328,7 +329,7 @@ fn test_marginal_2() {
         CPT::new(0, vec![], HashMap::from([
             (vec![0], Probability::Concrete(0.1)),
             (vec![1], Probability::Concrete(0.9))
-            ])), 
+            ])),
          CPT::new(1, vec![0], HashMap::from([
             (vec![0, 0], Probability::Concrete(0.1)),
             (vec![0, 1], Probability::Concrete(0.4)),
@@ -336,7 +337,7 @@ fn test_marginal_2() {
             (vec![1, 0], Probability::Concrete(0.3)),
             (vec![1, 1], Probability::Concrete(0.5)),
             (vec![1, 2], Probability::Concrete(0.2)),
-            ])), 
+            ])),
     ];
     let bn = BayesianNetwork::new(shape, cpts);
     let mut compiled = CompiledBayesianNetwork::new(&bn, CompileMode::BottomUpChaviraDarwicheBDD);
@@ -355,11 +356,11 @@ fn test_marginal_3() {
         CPT::new(0, vec![], HashMap::from([
             (vec![0], Probability::Concrete(0.1)),
             (vec![1], Probability::Concrete(0.9))
-            ])), 
+            ])),
         CPT::new(2, vec![], HashMap::from([
             (vec![0], Probability::Concrete(0.2)),
             (vec![1], Probability::Concrete(0.8))
-            ])), 
+            ])),
          CPT::new(1, vec![0, 2], HashMap::from([
             (vec![0, 0, 0], Probability::Concrete(0.1)),
             (vec![0, 0, 1], Probability::Concrete(0.4)),
@@ -367,12 +368,12 @@ fn test_marginal_3() {
             (vec![1, 0, 1], Probability::Concrete(0.5)),
             (vec![1, 1, 0], Probability::Concrete(0.15)),
             (vec![1, 1, 1], Probability::Concrete(0.85)),
-            ])), 
+            ])),
     ];
     let bn = BayesianNetwork::new(shape, cpts);
     let mut compiled = CompiledBayesianNetwork::new(&bn, CompileMode::BottomUpChaviraDarwicheBDD);
     let r = compiled.joint_marginal(&SymbolTable::empty(), &vec![0, 1, 2]);
 
-    assert_eq!(r[&vec![0, 0, 0]], 0.1 * 0.2 * 0.1);
-    assert_eq!(r[&vec![1, 0, 1]], 0.9 * 0.8 * 0.15);
+    assert_approx_eq!(r[&vec![0, 0, 0]], 0.1 * 0.2 * 0.1, 1e-3);
+    assert_approx_eq!(r[&vec![1, 0, 1]], 0.9 * 0.8 * 0.15, 1e-3);
 }
